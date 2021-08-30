a9610e20e21f0f4818d9c478eda196bb55659a80
zhuolu <yuzhongnan@come-future.com>
2021-08-30 15:52:27 +0800

    指定参数名

--- demo/src/main/java/com/zhuolu/demo/service/TestService.java
+++ demo/src/main/java/com/zhuolu/demo/service/TestService.java
@@ -1,5 +1,9 @@
 package com.zhuolu.demo.service;
 
+import java.util.List;
+
 public interface TestService {
     String sayHello(String name);
+
+    String sayHello(List<String> names);
 }
--- demo/src/main/java/com/zhuolu/demo/service/impl/TestServiceImpl.java
+++ demo/src/main/java/com/zhuolu/demo/service/impl/TestServiceImpl.java
@@ -3,10 +3,21 @@ package com.zhuolu.demo.service.impl;
 import com.zhuolu.demo.service.TestService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service("testService")
 public class TestServiceImpl implements TestService {
     @Override
     public String sayHello(String name) {
         return "fuck you " + name;
+    }
+
+    @Override
+    public String sayHello(List<String> names) {
+        StringBuilder sb = new StringBuilder();
+        for (String name : names) {
+            sb.append(sayHello(name)).append("\n");
+        }
+        return sb.toString();
     }
 }
