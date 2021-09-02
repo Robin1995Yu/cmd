aa13b2911f4bc8ddbb92c6bc786c0e73569cfd87
Robin1995Yu <robinyuchina@icloud.com>
2021-09-02 22:36:36 +0800

    完成json集合生成并测试 之后继续完成map的

--- demo/src/main/java/com/zhuolu/demo/service/TestService.java
+++ demo/src/main/java/com/zhuolu/demo/service/TestService.java
@@ -7,4 +7,5 @@ public interface TestService {
     String selectTest(TestBean1 testBean1);
     String selectTest(TestBean2 testBean2);
     String sayHello(String name);
+    Runnable getRunnable(String s);
 }
